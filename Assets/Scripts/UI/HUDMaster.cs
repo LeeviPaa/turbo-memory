b61cff2c2729aa6bc9cdc7d57f7a2a723abb38ff
Cudl <tossubot@gmail.com>
2021-01-30 10:40:55 +0200

    Add base font and make it default.

---
+++ Assets/Scripts/UI/HUDMaster.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDMaster : MonoBehaviour
+{
+    private GameManager _manager;
+
+    public void Awake()
+    {
+
+    }
+}
