24e795887dcb3300ab7dd59b5ba07bcbfea54b04
Cudl <tossubot@gmail.com>
2021-01-30 11:47:08 +0200

    UI Element stuff.

--- Assets/Scripts/UI/HUDMaster.cs
+++ Assets/Scripts/UI/HUDMaster.cs
@@ -1,13 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.Events;
 
-public class HUDMaster : MonoBehaviour
+public class HUDMaster : MonoBehaviourPunCallbacks
 {
-    private GameManager _manager;
+    private Photon.Realtime.Room _currentRoom;
+    public Photon.Realtime.Room GetCurrentRoom() => _currentRoom == null ? PhotonNetwork.CurrentRoom : _currentRoom;
 
-    public void Awake()
+    public UnityEvent<Photon.Realtime.Room> OnRoomUpdated = new UnityEvent<Photon.Realtime.Room>();
+    public UnityEvent<Player> OnPlayerJoined = new UnityEvent<Player>();
+    public UnityEvent<Player> OnPlayerLeft = new UnityEvent<Player>();
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
     {
+        base.OnPlayerEnteredRoom(newPlayer);
+        OnRoomUpdated.Invoke(GetCurrentRoom());
+    }
 
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        base.OnPlayerLeftRoom(otherPlayer);
+        OnRoomUpdated.Invoke(GetCurrentRoom());
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        OnRoomUpdated.Invoke(GetCurrentRoom());
     }
 }
